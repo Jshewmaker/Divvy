26ea32f77013f8a016fa7afda765340d398b55b0
srobrien1840 <srobrien@umich.edu>
2020-11-23 15:10:04 -0500

    fixed isbusinessUser null

--- lib/sila/repositories/sila_api_client.dart
+++ lib/sila/repositories/sila_api_client.dart
@@ -73,8 +73,9 @@ class SilaApiClient {
   /// Requires the user handle and the UserModel to fill out body
   ///
   Future<RegisterResponse> register(String handle, UserModel user,
-      {bool isbusinessUser}) async {
+      {bool isbusinessUser = false}) async {
     int utcTime = DateTime.now().millisecondsSinceEpoch;
+
     String address = await eth.createEthWallet(isBusinessUser: isbusinessUser);
 
     Map body = {
