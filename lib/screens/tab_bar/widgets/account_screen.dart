6ff099f3ea1247220331948478942e19e1570c96
joshuashewmaker <jshewmak@gmail.com>
2020-11-26 12:21:18 -0500

    removed debug code from account screens

--- lib/screens/tab_bar/widgets/account_screen.dart
+++ lib/screens/tab_bar/widgets/account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:divvy/screens/screens/account/account_options.dart';
 import 'package:divvy/screens/screens/account/check_handle_screen.dart';
+import 'package:divvy/screens/screens/account/plaid_link_screen.dart';
 import 'package:divvy/screens/screens/bank_account_balance_screen.dart';
 import 'package:divvy/screens/screens/get_wallet_info_screen.dart';
 import 'package:divvy/screens/screens/issue_sila_screen.dart';
@@ -18,30 +19,35 @@ class AccountScreen extends StatelessWidget {
               onPressed: () => Navigator.of(context).push(MaterialPageRoute(
                   builder: (contest) => AccountOptionsScreen())),
             ),
-            RaisedButton(
-              child: Text('Select Username'),
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (contest) => RegisterHandleScreen())),
-            ),
-            RaisedButton(
-              child: Text('get balance'),
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (contest) => GetWalletInfoScreen())),
-            ),
+            // RaisedButton(
+            //   child: Text('Select Username'),
+            //   onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+            //       builder: (contest) => RegisterHandleScreen())),
+            // ),
+            // RaisedButton(
+            //   child: Text('get balance'),
+            //   onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+            //       builder: (contest) => GetWalletInfoScreen())),
+            // ),
             RaisedButton(
               child: Text('Check Bank Account Balance'),
               onPressed: () => Navigator.of(context).push(MaterialPageRoute(
                   builder: (contest) => BankAccountInfoScreen())),
             ),
+            // RaisedButton(
+            //   child: Text('Issue Sila'),
+            //   onPressed: () => Navigator.of(context).push(
+            //       MaterialPageRoute(builder: (contest) => IssueSilaScreen())),
+            // ),
+            // RaisedButton(
+            //   child: Text('Get Transactions'),
+            //   onPressed: () => Navigator.of(context).push(
+            //       MaterialPageRoute(builder: (contest) => TransactionScreen())),
+            // ),
             RaisedButton(
-              child: Text('Issue Sila'),
+              child: Text('Link Plaid'),
               onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(builder: (contest) => IssueSilaScreen())),
-            ),
-            RaisedButton(
-              child: Text('Get Transactions'),
-              onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(builder: (contest) => TransactionScreen())),
+                  MaterialPageRoute(builder: (contest) => PlaidLinkScreen())),
             ),
           ],
         ),
