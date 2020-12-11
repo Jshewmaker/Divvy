8028602221ef2a35f3ea214a7432dab8e413eb29
srobrien1840 <srobrien@umich.edu>
2020-12-10 20:07:38 -0500

    merge conflicts

--- lib/screens/tab_bar/widgets/project_screen.dart
+++ lib/screens/tab_bar/widgets/project_screen.dart
@@ -10,8 +10,6 @@ import 'package:divvy/bloc/project/project_state.dart';
 
 import 'dart:math' as math;
 
-import 'package:divvy/screens/screens/line_item_info_screen.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jiffy/jiffy.dart';
