14583505573cd2016117d891244622df50ed30b2
joshuashewmaker <jshewmak@gmail.com>
2020-12-17 22:13:12 -0500

    refactored invoice code

--- lib/screens/screens/invoice_screen.dart
+++ lib/screens/screens/invoice_screen.dart
@@ -79,24 +79,8 @@ class InvoiceScreen extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text(
-                          _lineItem.title,
-                          style: TextStyle(
-                              fontSize: 42, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        cost,
-                        style: TextStyle(
-                            color: Colors.teal[400],
-                            fontSize: 48,
-                            fontWeight: FontWeight.bold),
-                      ),
+                      _Header(_lineItem),
+                      _Table(_lineItem),
                       SizedBox(
                         height: 20,
                       ),
@@ -106,122 +90,7 @@ class InvoiceScreen extends StatelessWidget {
                       SizedBox(
                         height: 20,
                       ),
-                      Table(
-                        columnWidths: {1: FractionColumnWidth(.35)},
-                        children: [
-                          TableRow(children: [
-                            Text(
-                              'Expected Finish Date: ',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: Text(
-                                DateFormat.yMMMMd('en_US').format(
-                                    _lineItem.expectFinishedDate.toDate()),
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ),
-                          ]),
-                          TableRow(children: [
-                            SizedBox(
-                              height: 20,
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                          ]),
-                          TableRow(children: [
-                            Text(
-                              'Work Submission Date: ',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: Text(
-                                DateFormat.yMMMMd('en_US').format(_lineItem
-                                    .generalContractorApprovalDate
-                                    .toDate()),
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ),
-                          ]),
-                          TableRow(children: [
-                            SizedBox(
-                              height: 20,
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                          ]),
-                          TableRow(children: [
-                            Text(
-                              'Payment & Approval Date: ',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: Text(
-                                paidDate(),
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ),
-                          ])
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(
-                        thickness: 3,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        height: 200,
-                        width: double.maxFinite,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0)),
-                          color: Colors.teal[200],
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                FittedBox(
-                                  fit: BoxFit.fitWidth,
-                                  child: Text(
-                                    'Payment of $cost from ',
-                                    style: TextStyle(
-                                        fontSize: 30, color: Colors.white),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  _project.homeownerSilaHandle.substring(6),
-                                  style: TextStyle(
-                                      fontSize: 22, color: Colors.white),
-                                ),
-                                Text(
-                                  ' to ',
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.white),
-                                ),
-                                Text(
-                                  _project.generalContractorSilaHandle
-                                      .substring(6),
-                                  style: TextStyle(
-                                      fontSize: 22, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
+                      _PaymentInfoCard(_project, _lineItem.cost),
                       SizedBox(
                         height: 30,
                       ),
@@ -244,6 +113,120 @@ class InvoiceScreen extends StatelessWidget {
               ),
             )));
   }
+}
+
+class _Header extends StatelessWidget {
+  _Header(this._lineItem);
+
+  final LineItem _lineItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final cost = NumberFormat.currency(symbol: '\$').format(_lineItem.cost);
+    return Column(
+      children: [
+        FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            _lineItem.title,
+            style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          cost,
+          style: TextStyle(
+              color: Colors.teal[400],
+              fontSize: 48,
+              fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Divider(
+          thickness: 3,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
+
+class _Table extends StatelessWidget {
+  _Table(this._lineItem);
+
+  final LineItem _lineItem;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Table(
+      columnWidths: {1: FractionColumnWidth(.35)},
+      children: [
+        TableRow(children: [
+          Text(
+            'Expected Finish Date: ',
+            style: TextStyle(fontSize: 18),
+          ),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              DateFormat.yMMMMd('en_US')
+                  .format(_lineItem.expectFinishedDate.toDate()),
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ]),
+        TableRow(children: [
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ]),
+        TableRow(children: [
+          Text(
+            'Work Submission Date: ',
+            style: TextStyle(fontSize: 18),
+          ),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              DateFormat.yMMMMd('en_US')
+                  .format(_lineItem.generalContractorApprovalDate.toDate()),
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ]),
+        TableRow(children: [
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ]),
+        TableRow(children: [
+          Text(
+            'Payment & Approval Date: ',
+            style: TextStyle(fontSize: 18),
+          ),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              paidDate(),
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ])
+      ],
+    );
+  }
 
   String paidDate() {
     if (_lineItem.datePaid == null) {
@@ -252,6 +235,54 @@ class InvoiceScreen extends StatelessWidget {
     } else {
       return DateFormat.yMMMMd('en_US').format(_lineItem.datePaid.toDate());
     }
+  }
+}
+
+class _PaymentInfoCard extends StatelessWidget {
+  _PaymentInfoCard(this._project, this._cost);
+  final Project _project;
+  final double _cost;
+  @override
+  Widget build(BuildContext context) {
+    final cost = NumberFormat.currency(symbol: '\$').format(_cost);
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      height: 200,
+      width: double.maxFinite,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        color: Colors.teal[200],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  'Payment of $cost from ',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                _project.homeownerSilaHandle.substring(6),
+                style: TextStyle(fontSize: 22, color: Colors.white),
+              ),
+              Text(
+                ' to ',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              Text(
+                _project.generalContractorSilaHandle.substring(6),
+                style: TextStyle(fontSize: 22, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
